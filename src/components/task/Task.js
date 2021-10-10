020555b2d9194a2a634e7d352e149d2aded0e7cf
Franco Jara <jarafranco122@gmail.com>
2021-10-09 23:55:05 -0300

    adicion de checkbutton y materialui

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -3,16 +3,21 @@ import './Task-styles.css'
 
 import { connect } from 'react-redux';
 import { removeTask, updateTask } from '../../actions/actions'
+import Checkbox from '@mui/material/Checkbox'
+// import Done from '@mui/icons-material/Done'
+import { CheckCircle, CheckCircleOutline } from '@mui/icons-material';
 
 function Task({ name, idTask, removeTask, updateTask }) {
 
     const [editing, setEditing] = useState(false)
+    const [crossedOut, setCrossedOut] = useState(false)
 
     // guardar estado localmente para luego enviarlo a la store
 
     const [newTaskName, setNewTaskName] = useState(name)
 
-    let handleName = name
+    let handleName = name;
+    let taskNameClassValue = "task-name";
 
     const saveNewTaskName = () => {
         setNewTaskName(handleName)
@@ -30,13 +35,18 @@ function Task({ name, idTask, removeTask, updateTask }) {
     function TaskName() {
         if (editing) {
             return (
-                <div>
-                    <input type="text" defaultValue={newTaskName} onChange={(e) => handleName = e.target.value} />
+                <div className="inputTaskContainer">
+                    <input className="inputTaskContainer__input" type="text" defaultValue={newTaskName} onChange={(e) => handleName = e.target.value} />
                 </div>
             )
         }
+
+        if (crossedOut) {
+            taskNameClassValue += ' crossedOutText'
+        }
+
         return (
-            <div className="task-name" onClick={() => setEditing(true)}>
+            <div className={taskNameClassValue} onClick={() => setEditing(true)}>
                 {name}
             </div>
         )
@@ -49,12 +59,16 @@ function Task({ name, idTask, removeTask, updateTask }) {
             )
         }
         return (
-            <button onClick={() => setEditing(true)}>Editar</button>
+            // <button onClick={() => setEditing(true)}>Editar</button>
+            <div>
+                
+            </div>
         )
     }
 
     return (
         <div className="task-container">
+            <Checkbox color="success" icon={<CheckCircleOutline />} checkedIcon={<CheckCircle />} onChange={() => {crossedOut ? setCrossedOut(false) : setCrossedOut(true)}} />
             <TaskName />
             <div className="buttons">
                 <button onClick={() => removeTask(idTask)}>Borrar</button>
